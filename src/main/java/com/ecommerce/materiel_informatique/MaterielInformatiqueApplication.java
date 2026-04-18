ae69bf4c279b9984be1bc12b7f3b0d918e7353a9
asmae0o <asmaehatim54@gmail.com>
2026-04-18 11:42:18 +0100

    Initial commit: Project setup with Spring Initializr

---
+++ src/main/java/com/ecommerce/materiel_informatique/MaterielInformatiqueApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.materiel_informatique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaterielInformatiqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaterielInformatiqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/materiel_informatique/MaterielInformatiqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.materiel_informatique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaterielInformatiqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
